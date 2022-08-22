031e5a1d6179be9fcf13383fabb5c51eee8b2af1
LEE JI HUN <111562001+200me@users.noreply.github.com>
2022-08-22 22:44:00 +0900

    Update create a tree.py

--- create a tree.py
+++ create a tree.py
@@ -1,10 +1,14 @@
-user_input1 = int(input("Please enter the number of lines. :"))
+# Output from the left
+#for i in range(1, user_input1+1):
+    #print("*"*i)
+   
+#Output from the right
+#for i in range(1, user_input1+1):
+    #print(" "*(user_input1 - i) + ('*'*i))
 
-for i in range(1, user_input1+1):
-    print("*"*i)
-                  
-for i in range(1, user_input1+1):
-    print(" "*(user_input1 - i) + ('*'*i))
+#Create a tree
+    
+user_input1 = int(input("Please enter the number of lines. :"))
 
 for i in range(1, user_input1 +1):
     print(" " *(user_input1 - i) + ('*' * (2*i -1)))
