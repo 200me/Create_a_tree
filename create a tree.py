68d9e210423fe53c5ab5dfee4776fbad3415aa33
LEE JI HUN <111562001+200me@users.noreply.github.com>
2022-08-22 22:39:14 +0900

    Add files via upload

---
+++ create a tree.py
@@ -0,0 +1,42 @@
+user_input1 = int(input("줄 수를 입력해주세요. :"))
+
+for i in range(1, user_input1+1):
+    print("*"*i)
+                  
+for i in range(1, user_input1+1):
+    print(" "*(user_input1 - i) + ('*'*i))
+
+for i in range(1, user_input1 +1):
+    print(" " *(user_input1 - i) + ('*' * (2*i -1)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
+
